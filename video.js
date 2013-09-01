d1633b6c3e1a571adb908574a45686b9be9a3b38
Achal Dave <achalddave@live.com>
2013-09-02 00:18:05 +0100

    explicitly avoid getting audio

--- video.js
+++ video.js
@@ -1,6 +1,6 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 window.URL = window.URL || window.webkitURL;
-navigator.getUserMedia({video: true}, function(localMediaStream) { 
+navigator.getUserMedia({video: true, audio: false}, function(localMediaStream) { 
   var video = document.getElementById("v"); 
   video.autoplay = true;
   video.src = window.URL.createObjectURL(localMediaStream);
